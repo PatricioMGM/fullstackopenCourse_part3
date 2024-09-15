3032a54e0b2ba9adb140ae1011340a9e811e9a29
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-15 02:45:56 -0600

    part 3.8

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 /* const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
