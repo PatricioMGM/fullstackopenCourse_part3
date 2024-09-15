c0eea3b37553a20a4dc245bb4a0713e607fc7dc9
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-15 15:01:06 -0600

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 let persons = [
   { 
@@ -28,6 +29,7 @@ let persons = [
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 /* const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
