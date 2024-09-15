913a17e19062f7bc652edc20fd03302155c860df
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-15 02:02:18 -0600

    part 3.7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 let persons = [
   { 
@@ -25,6 +26,19 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+/* const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+
+app.use(requestLogger) */
+
 
 app.get('/info', (request, response)=>{
   const html = 'Phonebook has info for ' + persons.length + ' persons <br/>' + new Date() 
@@ -88,6 +102,14 @@ app.post('/api/persons', (request, response)=>{
   persons = persons.concat(person)
   response.json(person)
 })
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
